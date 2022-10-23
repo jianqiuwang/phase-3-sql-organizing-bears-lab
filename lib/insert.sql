fc5123cbca1da1dc137c92acc7cade2badbfd83a
Jianqiu Wang <jianqiuwang1@icloud.com>
2022-10-23 16:50:39 -0500

    commit tree 95648c713bc880e2554a760c5d197b68ad69e0c7

---
+++ lib/insert.sql
@@ -0,0 +1,14 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES 
+    ("Mr. Chocolate", 1, "M", "Brown", "Good", 1),
+    ("Rowdy", 2, "M", "Grey", "Good", 1),
+    ("Tabitha", 3, "F", "Black", "Bad", 1),
+    ("Sergeant Brown", 4, "M", "Brown", "Good", 1),
+    ("Melissa", 5, "F", "White", "Good", 1),
+    ("Grinch", 6, "M", "Black", "Very Bad", 1),
+    ("Wendy", 7, "F", "Brown", "Good", 1),
+    (NULL, 8, "F", "Black", "Good", 0)
+    
+
+
+     
